d5411d3da88506b43f6dd0631469ed9320827746
Ashmitha Sowmya <76016259+Gashu8697@users.noreply.github.com>
2023-04-28 21:28:12 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,14 @@
+package com.LabAssignmentDsa1.com;
+
+public class Node {
+	Node left;
+	int data;
+	Node right;
+	
+	public Node(int data)
+	{
+		this.left=null;
+		this.data=data;
+		this.right=null;
+	}
+}
